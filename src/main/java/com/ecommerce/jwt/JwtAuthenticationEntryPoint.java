819c79bff9a9c1fe49279ce4f950dd7ff1d690eb
Kishore <kishore.dubey84@gmail.com>
2020-02-29 14:05:12 +0800

    improved error message for token related errors

--- src/main/java/com/ecommerce/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/ecommerce/jwt/JwtAuthenticationEntryPoint.java
@@ -14,6 +14,6 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Se
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authorisation header is either missing or expired in the header");
     }
 }
