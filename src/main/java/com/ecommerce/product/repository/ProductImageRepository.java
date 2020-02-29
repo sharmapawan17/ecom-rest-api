48300b25a6279e2abf5be907aaae9d2d96bdaf27
Kishore <kishore.dubey84@gmail.com>
2020-02-29 12:43:29 +0800

    updated link for product images

--- src/main/java/com/ecommerce/product/repository/ProductImageRepository.java
+++ src/main/java/com/ecommerce/product/repository/ProductImageRepository.java
@@ -4,7 +4,11 @@ import com.ecommerce.product.entity.ProductImageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository("productImageRepository")
 public interface ProductImageRepository extends JpaRepository<ProductImageEntity, Long> {
+
+    List<ProductImageEntity> findByProductId(long productId);
 }
 
